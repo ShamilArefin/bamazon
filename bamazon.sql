472b99204d114eae7eb27dc94b4dc884e0709ec3
Shamil Arefin <landoflitany@gmail.com>
2018-04-26 18:17:29 -0500

    1

---
+++ bamazon.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+create table products (
+item_id int not null auto_increment, 
+product_name varchar(250),
+department_name varchar(250),
+price decimal(10,2),
+stock_quantity int
+)
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("Shooting Star Press", "Finisher Moves", 5.00, 20);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("Super Kick!!!!!!", "Finisher Moves", 5.00, 20);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("One-Winged Angel", "Finisher Moves", 7.00, 10);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("Lace-Up Boots", "Attire", 100.00, 15);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("Basic Kickpads", "Attire", 60.00, 10);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("Basic Trunks", "Attire", 75.00, 15);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("Shorts", "Attire", 20.00, 15);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("Chokeslam", "Finisher", 5.00, 20);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("Clothesline", "Finisher", 5.00, 20);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values("Styles Clash", "Finisher", 7.00, 15);
